5c7f47808de85efaff885e7d8b9d20217c71c488
samandargofurov <samandargofurov236@gmail.com>
2024-03-12 21:00:22 +0500

    3 lesson

---
+++ src/App.jsx
@@ -0,0 +1,24 @@
+// import { useState } from 'react'
+// import './App.css'
+
+
+// function App() {
+//   const [isTurn, setisTurn] = useState(false)
+
+//   function isTurn() {
+//     return (
+//       <>
+
+//       </>
+//     )
+//   }
+
+//   return (
+//     <>
+//       <isTurn />
+//     </>
+//   )
+// }
+
+
+// export default App;
